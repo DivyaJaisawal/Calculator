43bbda226765cfd91d211ce50a0a16c8e8025e7a
Divya Jaisawal <divyajaisawal@go-jek.com>
2018-08-24 05:03:38 +0530

    [Divya] Adds + arithmetic operation.

---
+++ src/main/java/com/gojek/ArithmeticOperation.java
@@ -0,0 +1,10 @@
+package com.gojek;
+
+
+public class ArithmeticOperation {
+
+    public int add(int firstVal, int secondVal) {
+        return firstVal + secondVal;
+    }
+
+}
---
+++ src/test/java/com/gojek/ArithmeticOperationTest.java
@@ -0,0 +1,38 @@
+package com.gojek;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ArithmeticOperationTest {
+
+    ArithmeticOperation arithmeticOperation = null;
+
+    @Before
+    public void setUp() throws Exception {
+        arithmeticOperation = new ArithmeticOperation();
+    }
+
+    @Test
+    public void testShouldReturnZeroIfBothTheInputsAreZero() throws Exception {
+        assertEquals(0, arithmeticOperation.add(0,0));
+    }
+
+    @Test
+    public void testShouldReturnFirstNumberIfSecondNumIsZero() throws Exception {
+        assertEquals(3, arithmeticOperation.add(3, 0));
+    }
+
+    @Test
+    public void testShouldReturnSecondNumberIfFirstNumIsZero() throws Exception {
+        assertEquals(5, arithmeticOperation.add(0, 5));
+
+    }
+
+    @Test
+    public void testShouldReturnSumIfBothNumberIsPresent() throws Exception {
+        assertEquals(10, arithmeticOperation.add(5, 5));
+    }
+}
